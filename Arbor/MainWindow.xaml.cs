49807e09ab32595ec11228837e875dfc36d01744
Bia Beniamin <Bia Beniamin>
2016-11-06 21:52:13 +0200

    project created

--- Arbor/MainWindow.xaml.cs
+++ Arbor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace Arbor
         public MainWindow()
         {
             InitializeComponent();
+            Person p = new Person(1, "", "");
         }
     }
 }
---
+++ Arbor/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbor
+{
+    public class Person
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        private Person _mother;
+        private Person _father;
+
+        public Person MyProperty
+        {
+            get { return _father; }
+            set { _father = value; }
+        }
+
+        public Person Mother
+        {
+            get { return _mother; }
+            set { _mother = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public Person(int id,string name,string description):this(id,name,description,null,null)
+        {
+
+        }
+        public Person(int id, string name, string description,Person father,Person mother)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+            _father = father;
+            _mother = mother;
+        }
+    }
+}
